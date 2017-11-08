623564ad96b2a62ccd33a03f91c29cd7d1779fba
Alex Sendula <33348550+M1keWazowski@users.noreply.github.com>
2017-11-08 09:50:35 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
    
-   $db = new mysqli("localhost", "deb43619_alex", "Password", "deb43619_alex");
+   $db = new mysqli("servername", "username", "password", "dbname");
   
 if (mysqli_connect_errno()) { 
     printf("Connect failed: %s\n", mysqli_connect_error()); 
